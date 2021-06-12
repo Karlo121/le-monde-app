7e68843945458ac8b3cc0fe27176b20eabd4f707
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-06-12 15:22:20 +0200

    validacija done

--- unos.php
+++ unos.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>" />
   <title>Document</title>
 
 </head>
@@ -25,35 +25,41 @@
   </div>
   <div class="postWrapper">
     <div class="formWrapper">
-      <form enctype="multipart/form-data" action="query.php" method="POST">
+      <form enctype="multipart/form-data" action="query.php" method="POST" onSubmit="validacija()">
         <div class="formItem">
+          <span id="porukaTitle" class="errorText"></span>
           <label for="title">Naslov vijesti</label>
           <div class="formField">
-            <input type="text" name="title" class="formField-textual" />
+            <input type="text" name="title" id="title" class="formField-textual" />
           </div>
         </div>
         <div class="formItem">
+          <span id="porukaAbout" class="errorText"></span>
           <label for="about">Kratki sadržaj vijesti (do 50 znakova)</label>
           <div class="formField">
-            <textarea name="about"></textarea>
+            <textarea name="about" id="about"></textarea>
           </div>
         </div>
         <div class="formItem">
+          <span id="porukaContent" class="errorText"></span>
           <label for="content">Sadržaj vijesti</label>
           <div class="formField">
-            <textarea name="content"></textarea>
+            <textarea name="content" id="content"></textarea>
           </div>
         </div>
         <div class="formItem">
+          <span id="porukaSlika" class="errorText"></span>
           <label for="pphoto">Slika: </label>
           <div class="formField">
-            <input type="file" accept="image/jpg,image/gif" class="input-text" name="pphoto" />
+            <input type="file" accept="image/jpg,image/gif" class="input-text" name="pphoto" id="pphoto" />
           </div>
         </div>
         <div class="formItem">
+          <span id="porukaKategorija" class="errorText"></span>
           <label for="category">Kategorija vijesti</label>
           <div class="formField">
-            <select name="category" id="" class="formFieldTextual">
+            <select name="category" id="category" class="formFieldTextual">
+              <option value=""></option>
               <option value="politika">Politika</option>
               <option value="sport">Sport</option>
               <option value="kultura">Kultura</option>
@@ -69,7 +75,7 @@
         </div>
         <div class="formItem">
           <button type="reset" value="Poništi">Poništi</button>
-          <button type="submit" value="Prihvati">Prihvati</button>
+          <button type="submit" value="Prihvati" id="slanje">Prihvati</button>
         </div>
       </form>
     </div>
@@ -80,6 +86,70 @@
     <p>Suevez le Mond</p>
   </div>
 </body>
+<script>
+document.getElementById("slanje").onclick = function(event) {
+  let slanjeForme = true;
 
+  let poljeTitle = document.getElementById("title");
+  let title = document.getElementById("title").value;
+
+  if (title.length < 5 || title.length > 30) {
+    slanjeForme = false;
+    poljeTitle.style.border = "1px dashed red";
+    document.getElementById("porukaTitle").innerHTML = "Naslov vjesti mora imati između 5 i 30 znakova!<br>";
+  } else {
+    poljeTitle.style.border = "1px solid green";
+    document.getElementById("porukaTitle").innerHTML = "";
+  }
+
+
+  let poljeAbout = document.getElementById("about");
+  let about = document.getElementById("about").value;
+  if (about.length < 10 || about.length > 100) {
+    slanjeForme = false;
+    poljeAbout.style.border = "1px dashed red";
+    document.getElementById("porukaAbout").innerHTML = "Kratki sadržaj mora imati između 10 i 100 znakova!<br>";
+  } else {
+    poljeAbout.style.border = "1px solid green";
+    document.getElementById("porukaAbout").innerHTML = "";
+  }
+
+  let poljeContent = document.getElementById("content");
+  let content = document.getElementById("content").value;
+  if (content.length == 0) {
+    slanjeForme = false;
+    poljeContent.style.border = "1px dashed red";
+    document.getElementById("porukaContent").innerHTML = "Sadržaj mora biti unesen!<br>";
+  } else {
+    poljeContent.style.border = "1px solid green";
+    document.getElementById("porukaContent").innerHTML = "";
+  }
+
+  let poljeSlika = document.getElementById("pphoto");
+  let pphoto = document.getElementById("pphoto").value;
+  if (pphoto.length == 0) {
+    slanjeForme = false;
+    poljeSlika.style.border = "1px dashed red";
+    document.getElementById("porukaSlika").innerHTML = "Slika mora biti unesena!<br>";
+  } else {
+    poljeSlika.style.border = "1px solid green";
+    document.getElementById("porukaSlika").innerHTML = "";
+  }
+
+  let poljeCategory = document.getElementById("category");
+  if (document.getElementById("category").selectedIndex == 0) {
+    slanjeForme = false;
+    poljeCategory.style.border = "1px dashed red";
+    document.getElementById("porukaKategorija").innerHTML = "Kategorija mora biti odabrana!<br>";
+  } else {
+    poljeCategory.style.border = "1px solid green";
+    document.getElementById("porukaKategorija").innerHTML = "";
+  }
+
+  if (slanjeForme != true) {
+    event.preventDefault();
+  }
+}
+</script>
 
 </html>
